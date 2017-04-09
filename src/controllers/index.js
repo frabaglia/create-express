70b031fc34ba5271f10550909abf5b45a25ad14e
Franco Rabaglia <francoo.hd@gmail.com>
2017-04-08 21:33:55 -0300

    fixing production return

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
+
     var options = {
-        root: __dirname + '/../views/',
+        root: __dirname + '/../../views/',
         dotfiles: 'deny',
         headers: {
             'x-timestamp': Date.now(),
@@ -8,9 +9,12 @@ module.exports = (req, res, next) => {
         }
     };
 
+    console.log(options.root);
+
     res.sendFile("index.html", options, function(err) {
         if (err) {
             let err = new Error("Can't open index file.")
+            console.log(JSON.stringify(err));
             err.status = 500
             next(err)
         }
